9b8291840f8701924871a438d9d6c21373f34d54
Li Wenhua <lwenhua@zhijin.com>
2018-01-15 15:00:55 +0800

    first

---
+++ CaipiaoApp/CaipiaoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CaipiaoApp
+//
+//  Created by 李文华 on 2018/1/15.
+//  Copyright © 2018年 李文华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CaipiaoApp/CaipiaoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CaipiaoApp
+//
+//  Created by 李文华 on 2018/1/15.
+//  Copyright © 2018年 李文华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
